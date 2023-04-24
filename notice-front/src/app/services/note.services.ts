832fde509c359a4e1fc4b90bca6729c35ba29aaf
AlexDeng911 <pentod@gmail.com>
2023-04-24 16:59:20 +0300

    monday 24/04/23 work

--- notice-front/src/app/services/note.services.ts
+++ notice-front/src/app/services/note.services.ts
@@ -1,34 +1,45 @@
 import {Injectable} from "@angular/core";
 import {Apollo} from "apollo-angular";
 import {GetNotesQl} from "../queries/GetNotesQl";
-import {Observable, pluck} from "rxjs";
-import {ApolloQueryResult} from "@apollo/client/core";
+import {map, Observable, pluck} from "rxjs";
 import {NoteInterface} from "../interfaces/note.interface";
+import {CreateNoteQL} from "../queries/CreateNotesQL";
+
+interface CreateNoteResponse {
+  createNote: NoteInterface;
+}
+
+interface getNoteQL {
+  notes: NoteInterface[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class NoteServices{
+export class NoteServices {
   private noteServices: any;
 
-  constructor (private apollo: Apollo, private getNoteQl: GetNotesQl){}
+  constructor(private apollo: Apollo,
+              private getNoteQl: GetNotesQl,
+              private createNoteQL: CreateNoteQL) {
+  }
 
-  getNotes(): Observable<NoteInterface[]>{
+  getNotes(): Observable<NoteInterface[]> {
     return this.apollo.watchQuery<any>({
       query: this.getNoteQl.document,
       fetchPolicy: 'network-only'
-    }).valueChanges.pipe(pluck('data','getNotes'));
+    }).valueChanges.pipe(pluck('data', 'getNotes'));
   }
 
-  createNote(): {
-
+  createNote(note: NoteInterface): Observable<NoteInterface> {
+    return this.apollo.mutate<any, NoteInterface>({
+      mutation: this.createNoteQL.document,
+      fetchPolicy: 'network-only'
+    }).pipe(
+      map(response => response.data.createNote)
+    );
   }
-
 }
 
-// delete(id:string) {
-//   this.noteServices.delete(id).subscribe(()=>{
-//     this.getNotes();
-//   })
-// }
+
